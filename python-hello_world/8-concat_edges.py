1e23d3bd2396025dd31574f17719b97bba57849a
SilverDontAsk <steven_rosario@penguin>
2024-05-07 11:16:55 -0400

    this one is p tough

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+language that combines remarkable power with very clear syntax"
 
-print(" ".join(str.split()[5:8]).lower() + " with Python")
+print(str[39:66] + " with " + str[0:6])
