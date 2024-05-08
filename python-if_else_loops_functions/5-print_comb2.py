382e0e8694f98ecb0d9aa22576f355915cacc4b2
SilverDontAsk <steven_rosario@penguin>
2024-05-08 13:54:13 -0400

    maybe good

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-import sys
 for i in range(100):
-    print("{:02}, ".format(i), end='')
+    if i < 99:
+        print("{:02}, ".format(i), end='')
+    else:
+        print("{:02}".format(i), end='\n')
+        break
