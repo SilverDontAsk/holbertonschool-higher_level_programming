32c471f9e629d9aee233f9a8cc678afa62e72538
SilverDontAsk <steven_rosario@penguin>
2024-05-08 14:10:15 -0400

    this should be good

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i * 10 + j == 89:
+            print("{:02}".format(89))
+            break
+        print("{:02}, ".format(i * 10 + j), end="")
+    else:
+        continue
+    break
