4b3c619a5bb38a9aaeb4a56a60788e8054574928
SilverDontAsk <steven_rosario@penguin>
2024-05-10 14:13:25 -0400

    idk

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import importlib.util
 
+def load_variable():
+    file_path = 'variable_load_5.py'
+    spec = importlib.util.spec_from_file_location('module_name', file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.a
 
-file_path = 'variable_load_5.py'
 
-spec = importlib.util.spec_from_file_location('module_name', file_path)
-
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
-print(module.a)
+if __name__ == "__main__":
+    value_of_a = load_variable()
+    print(value_of_a)
