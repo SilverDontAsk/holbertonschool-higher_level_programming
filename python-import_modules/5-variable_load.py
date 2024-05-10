f86cecc9f7d49b377fe559aea8de2d145bfd642d
SilverDontAsk <steven_rosario@penguin>
2024-05-10 14:10:32 -0400

    idk

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -2,15 +2,11 @@
 import importlib.util
 
 
-def import_variable(module_name, variable_name):
-    spec = importlib.util.spec_from_file_location(module_name, "variable_load_5.py")
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    if hasattr(module, variable_name):
-        value = getattr(module, variable_name)
-        print(value)
-    else:
-        print(f"Variable '{variable_name}' not found in {module_name}.py")
+file_path = 'variable_load_5.py'
 
-if __name__ == "__main__":
-    import_variable("variable_load_5", "a")
+spec = importlib.util.spec_from_file_location('module_name', file_path)
+
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+print(module.a)
