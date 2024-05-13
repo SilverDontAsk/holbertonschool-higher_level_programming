5a967b9fcf8fc12d865d2805f4aade792e541a39
SilverDontAsk <steven_rosario@penguin>
2024-05-13 14:08:23 -0400

    This adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a[:2] + (0,) * (2 - len(tuple_a)) # these things basically force the tuples to be the same length if needed
+    tuple_b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
