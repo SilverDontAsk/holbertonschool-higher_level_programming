a615d294a3a013ff43d9283e4dc64185cd59a45a
SilverDontAsk <steven_rosario@penguin>
2024-05-17 11:28:52 -0400

    this should be good

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square 'size'.
+"""
+
+
+class Square:
+    """This is a class that defines a square by size"""
+    def __init__(self, size):
+        self.__size = size
