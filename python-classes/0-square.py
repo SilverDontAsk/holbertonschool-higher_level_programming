d96c6346c3f4ee22ef8433a16615fefb5fdcac82
SilverDontAsk <steven_rosario@penguin>
2024-05-17 11:02:46 -0400

    surely this documentation is good enough

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class 'Square'.
+Pass is used so it does not give errors
+"""
 class Square:
+    """This represents an empty square."""
     pass
