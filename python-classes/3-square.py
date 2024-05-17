0f2e8b09673bba22bf1f038ad9a73558cce37d9a
SilverDontAsk <steven_rosario@penguin>
2024-05-17 12:02:21 -0400

    This calculates the area of the square

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square 'size'.
+"""
+
+
+class Square:
+    """This is a class that defines a square by size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size >= 0:
+            self.__size = size
+        else:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """Calculates and returns area of square"""
+        return self.__size ** 2
