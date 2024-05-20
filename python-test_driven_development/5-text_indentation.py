05f6a518fa3256305dd8dd2a2dbdf6b5b7539917
SilverDontAsk <steven_rosario@penguin>
2024-05-20 19:07:10 -0400

    This should be good

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for c in '.?,:':
+        text = text.replace(c, f'{c}\n\n')
+
+    lines = text.split('\n')
+    for i, line in enumerate(lines):
+        if line:
+            print(line.strip())
+            if i < len(lines) - 1:
+                print()
