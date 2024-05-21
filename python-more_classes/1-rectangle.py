f37abd9a3dc7fab14dfbfd000f88656128e96566
SilverDontAsk <steven_rosario@penguin>
2024-05-21 10:53:59 -0400

    This might be good

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
     @property
     def height(self):
         """Retrieves the height of the rectangle"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -40,12 +40,12 @@ class Rectangle:
             raise TypeError("height must be an int")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
 
     @property
     def width(self):
         """Retrieves the width of the rectangle."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -63,7 +63,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     def __str__(self):
         """Returns a string representation of the rectangle."""
