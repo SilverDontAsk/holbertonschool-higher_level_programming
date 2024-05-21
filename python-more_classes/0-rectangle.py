458ec909380c23084fc6abc7cfbd38f072c1efbc
SilverDontAsk <steven_rosario@penguin>
2024-05-21 10:03:50 -0400

    I genuinely have no idea what might be wrong

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """Defines a rectangle
 
+Attributes:
+    width: width of the rectangle.
+    height: the height of the rectangle.
 
+Methods:
+    None.
 """
 class Rectangle:
     pass
