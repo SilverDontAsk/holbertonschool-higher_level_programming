f5fa61c7f769a81d6c4d63b8a7b85bf03a22ea4c
SilverDontAsk <steven_rosario@penguin>
2024-05-21 10:50:06 -0400

    idk

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,6 +11,8 @@ class Rectangle:
     width(self, value): sets the width of the rectangle
     height(self): retrieves the height of the rectangle
     height(self, value): sets the height of the rectangle
+    __str__(self): Returns a string representation of the rectangle.
+    __repr__(self): Returns a string representation of the rectangle.
 
     """
     def __init__(self, width=0, height=0):
@@ -18,32 +20,9 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        """Retrieves the width of the rectangle."""
-        return self._Rectangle_width
-
-    @width.setter
-    def width(self, value):
-        """Sets the width of the rectangle.
-
-        Args:
-            value: the width of the rectangle.
-
-        Raises:
-            TypeError: if value is not an int
-            ValueError: if value is less than 0
-
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self._Rectangle_width = value
-
-    @property
     def height(self):
         """Retrieves the height of the rectangle"""
-        return self._Rectangle_height
+        return self._height
 
     @height.setter
     def height(self, value):
@@ -61,4 +40,36 @@ class Rectangle:
             raise TypeError("height must be an int")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._Rectangle_height = value
+        self._height = value
+
+    @property
+    def width(self):
+        """Retrieves the width of the rectangle."""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle.
+
+        Args:
+            value: the width of the rectangle.
+
+        Raises:
+            TypeError: if value is not an int
+            ValueError: if value is less than 0
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return f"[Rectangle] ({self.width})/{self.height}"
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle."""
+        return f"Rectangle(_width={self._width}, _height={self._height})"
