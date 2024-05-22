604a2e2247543a3e4dfb51c45a3c16c209e46db7
SilverDontAsk <steven_rosario@penguin>
2024-05-22 11:28:39 -0400

    i assume this is what was missing

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines the loopup function that will return a list of
+available attributes and methods of an object.
+"""
+
 
 def lookup(obj):
     """Returns the list of available attributes and methods of obj
