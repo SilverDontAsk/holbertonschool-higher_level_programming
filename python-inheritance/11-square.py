fa27dcc2f8a79a95a10e1cffa04b8ddb167cfb06
SilverDontAsk <steven_rosario@penguin>
2024-05-22 14:54:07 -0400

    This is literally the same code as the last one...

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Thie module represents a class square that inherits from class Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that inherits from class rectangle
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        calclates the size of the square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        return square description
+        """
+        return f"[Square] {self.__size}/{self.__size}"
