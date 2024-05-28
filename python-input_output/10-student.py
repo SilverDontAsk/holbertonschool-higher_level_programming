2bda06b96346fc6f368481cbafd5ebf9a4d31f35
SilverDontAsk <steven_rosario@penguin>
2024-05-28 16:18:46 -0400

    .

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module with a Student class
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves the dictionary representation of an instance of
+        student.
+        """
+        if attrs is not None and isinstance(attrs, list):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
