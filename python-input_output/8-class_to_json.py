501396145bbffc84ed873bb8527ad2745cca0ae4
SilverDontAsk <steven_rosario@penguin>
2024-05-28 15:50:41 -0400

    .

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,6 +3,8 @@
 Module that returns the dictionary description with simple data structure
 for JSON serialization of an object
 """
+
+
 def class_to_json(obj):
     """
     Returns the dictionary description
