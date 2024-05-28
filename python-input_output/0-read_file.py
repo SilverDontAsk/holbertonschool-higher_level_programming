7a28196ab14f1eefcb6655139bdad4ed6ba31bd7
SilverDontAsk <steven_rosario@penguin>
2024-05-28 10:58:25 -0400

    .

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ Funtion to read a file and print it out to stdout
 
 
 def read_file(filename=""):
+    """
+    reads the file using a with function and prints it out to stdout
+    """
     try:
         with open(filename, 'r') as file:
             for line in file:
