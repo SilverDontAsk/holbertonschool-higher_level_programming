1812e23361e442aa55a28c7d9e11d83923cba302
SilverDontAsk <steven_rosario@penguin>
2024-05-29 14:30:28 -0400

    .

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module to serialize and deserialize with the pickle module
+"""
+import pickle
+
+
+class CustomObject:
+    """
+    Custom Object class with name, age, is_student
+    """
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        """
+        serializes with the pickle module
+        """
+        with open(filename, 'wb') as pickle_file:
+            pickle.dump(self, pickle_file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Deserializes with the pickle module
+        """
+        with open(filename, 'rb') as pickle_file:
+            return pickle.load(pickle_file)
