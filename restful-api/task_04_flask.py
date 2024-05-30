39496b9cb3d68793154a9fd1350e69949cdee1a7
SilverDontAsk <steven_rosario@penguin>
2024-05-30 13:56:16 -0400

    since for some odd reason I cannot really install and have the stuff working im just doing guess work

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Simple API in python with flask
+"""
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+@app.route('/')
+def home():
+    """
+    message displayed on home page
+    """
+    return "Welcome to the Flask API!"
+
+@app.route('/data')
+def get_data():
+    """
+    returns a jsonified list of users
+    """
+    return jsonify(list(users.keys()))
+
+@app.route('/status')
+def get_status():
+    """
+    gets the status of the API
+    """
+    return "OK"
+
+@app.route
+def get_user(username):
+    """
+    gets and jsonifies users
+    """
+    user_data = users.get(username)
+    if user_data:
+        return jsonify(user_data)
+    else:
+        return "User Not Found", 404
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    """
+    adds users
+    """
+    user_data = requests.get_json()
+    if user_data:
+        username = user_data.get('username')
+        users[username] = user_data
+        return jsonify(user_data), 201
+    else:
+        return "Invalid JSON data", 400
+
+if __name__ == "__main__":
+    app.run()
