536bd5c4446c2f6b08c841a363835a9392204aaf
SilverDontAsk <steven_rosario@penguin>
2024-05-30 13:24:10 -0400

    .

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module to set up a basic HTTP server
+"""
+import http.server
+import json
+from http import HTTPStatus
+
+
+class SimpleHTTPRequestHandler(http.server.Base.HTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(HTTPStatus.OK)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API")
+        elif self.path == '/data':
+            self.send_response(HTTPStatus.OK)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dump(data).encode('utf-8'))
+        elif self.path == '/status':
+            self.send_response(HTTPStatus.OK)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(HTTPStatus.NOT_FOUND)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found: Endpoint not found")
+
+
+def run(server_class=http.server.HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print("Starting httpd server on port 8000...")
+    httpd.serve_forever()
