2d5ce7dfc57c637ad8a4d3db9170acf41995c8a3
SilverDontAsk <steven_rosario@penguin>
2024-06-17 15:49:59 -0400

    .

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- creates a table called first_table
+/*!50100 TABLE 'first_table' */;
+/*!50101 CREATE TABLE 'first_table' (
+'id' int DEFAULT NULL,
+'name' varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci */
