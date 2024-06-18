bafc100771daee4c1b162083451fa81cac8d3284
SilverDontAsk <steven_rosario@penguin>
2024-06-18 16:43:31 -0400

    .

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows and their genres
 SELECT s.title AS title, g.name AS name
 FROM tv_shows s
-JOIN tv_show_genres sg ON s.id = sg.show_id
-JOIN tv_genres g ON sg.genre_id = g.id
+LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
 ORDER BY s.title ASC, g.name ASC;
