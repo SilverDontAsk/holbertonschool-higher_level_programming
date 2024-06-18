167febc3e0a7f84e397385ba2b3203da819cba61
SilverDontAsk <steven_rosario@penguin>
2024-06-18 16:27:39 -0400

    .

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- lists all the shows in the comedy genre
+SELECT s.title AS title
+FROM tv_shows s
+JOIN tv_show_genres sg ON s.id = sg.show_id
+JOIN tv_genres g ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC;
