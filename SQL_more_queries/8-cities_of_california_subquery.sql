7064d2bc031b8cd4df5a72f042f306aecdd5413c
SilverDontAsk <steven_rosario@penguin>
2024-06-18 14:19:02 -0400

    .

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- list all cities of California in db hbtn_0d_usa
-SELECT cities.name
+SELECT cities.id, cities.name
 FROM cities, states
 WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
