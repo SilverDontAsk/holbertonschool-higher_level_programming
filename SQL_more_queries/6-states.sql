552df0ee3c2c945c4799ecd8fbdad37792662360
SilverDontAsk <steven_rosario@penguin>
2024-06-18 13:17:28 -0400

    .

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,11 @@
 -- creates database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- switch to newly created db
+USE hbtn_0d_usa;
+
 -- create table states in previously created database
-CREATE TABLE IF NOT EXISTS states to hbtn_0d_usa.* (
+CREATE TABLE IF NOT EXISTS states (
 	id INT PRIMARY KEY UNIQUE NOT NULL,
 	name VARCHAR(256) NOT NULL
 	);
