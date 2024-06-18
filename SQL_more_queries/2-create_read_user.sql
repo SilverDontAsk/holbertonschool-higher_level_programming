22116e4af4de4b465c54a3f3485ed8b8c15eb28b
SilverDontAsk <steven_rosario@penguin>
2024-06-18 12:19:50 -0400

    .

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grants select privilege on hbtn_0d_2 to user_0d_2
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- flush privileges
 FLUSH PRIVILEGES;
