91f21b6ac4fedb9bd5577804c423385ef7b81d6b
SilverDontAsk <steven_rosario@penguin>
2024-06-18 13:19:06 -0400

    .

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,6 +6,6 @@ USE hbtn_0d_usa;
 
 -- create table states in previously created database
 CREATE TABLE IF NOT EXISTS states (
-	id INT PRIMARY KEY UNIQUE NOT NULL,
+	id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
 	name VARCHAR(256) NOT NULL
 	);
