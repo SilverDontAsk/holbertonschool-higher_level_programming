966ed3c9c5527ad460e83d3b949153cdd2ce454e
SilverDontAsk <steven_rosario@penguin>
2024-06-20 19:54:05 -0400

    everything works fine, i have a feeling something is wrong(probably in requirements) but at least it works

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This script will take an argument and display all
+values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+import MySQLdb
+import sys
+
+
+def filter_by_input(username, password, database, state):
+    """
+    filters the input and sets the connection to the
+    database
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+    select = db.cursor()
+    query = "SELECT id, name FROM states WHERE name = %s ORDER BY id ASC"
+    select.execute(query, (state, ))
+    rows = select.fetchall()
+
+    for row in rows:
+        print(row)
+
+    select.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    filter_by_input(username, password, database, state)
