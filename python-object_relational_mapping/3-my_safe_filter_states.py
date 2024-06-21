5f3aa0e1361fd9a3fb26ec740dbe62d92632ba1d
SilverDontAsk <steven_rosario@penguin>
2024-06-20 20:42:12 -0400

    removed something unecessary

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -31,9 +31,6 @@ def filter_by_input(username, password, database, state):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state>".format(sys.argv[0]))
-        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
