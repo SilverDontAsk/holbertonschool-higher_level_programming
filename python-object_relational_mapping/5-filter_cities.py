2403bfd898762d09fce37788cc3eaa314b7f5725
SilverDontAsk <steven_rosario@penguin>
2024-06-20 21:39:35 -0400

    this looks fine on terminal, but once again, Idk if there might be any issues but we'll see

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+takes the name of a state as an argument and lists
+the cities of that state using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+
+def list_cities_by_input(username, password, database, state):
+    """
+    sets up a connection to hbtn_0e_4_usa and grab an input
+    to list cities off of
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+    select = db.cursor()
+    qr = """
+    SELECT GROUP_CONCAT(name SEPARATOR ', ')
+    FROM cities
+    WHERE state_id IN (SELECT id FROM states WHERE name = %s)
+    ORDER BY id ASC
+    """
+    select.execute(qr, (state, ))
+
+    cities_string = select.fetchone()[0]
+    print(cities_string)
+
+    select.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    list_cities_by_input(username, password, database, state)
