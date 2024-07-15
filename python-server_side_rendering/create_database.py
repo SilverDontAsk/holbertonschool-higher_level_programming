39d00de81204a6d6664e2c512a8711313a6cbf12
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-15 14:44:55 -0800

    .

--- python-server_side_rendering/create_database.py
+++ python-server_side_rendering/create_database.py
@@ -16,6 +16,7 @@ def create_database():
         VALUES
         (1, 'Laptop', 'Electronics', 799.99),
         (2, 'Coffee Mug', 'Home Goods', 15.99)
+        (3, 'Jarvis', 'AI Assistant', 2999.99)
     ''')
     conn.commit()
     conn.close()
