cabc1b092a41c6dd7629dff4a415f9f88db138e6
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-26 14:18:56 -0800

    >

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,24 +1,18 @@
-const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
-
-fetch(url)
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-})
-.then(data => {
-    const movies = data.results;
+document.addEventListener('DOMContentLoaded', () => {
+    const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
     const listMovies = document.getElementById('list_movies');
 
-    movies.foreach(movie => {
-        const listItem = document.createElement('li');
-        listItem.textcontent = movie.title;
-        listMovies.appendChild(listItem);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const films = data.results;
+        films.forEach(film => {
+            const li = document.createElement('li');
+            li.textContent = film.title;
+            listMovies.appendChild(li);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching movie titles:', error);
     });
-})
-.catch(error => {
-    const listMovies = document.getElementById('list_movies');
-    listMovies.textContent = 'Failed to load movie titles';
-    console.error('There was a problem with the fetch operation:', error);
 });
